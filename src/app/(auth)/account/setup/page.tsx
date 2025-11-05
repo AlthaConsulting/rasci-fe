a5b2d7bba8e8a5d4298ce4fe25c20de05666fa92
naufal.surya <naufal.surya@phiraka.com>
2025-11-05 16:26:15 +0700

    fix(deployment): configure file fixing

--- src/app/(auth)/account/setup/page.tsx
+++ src/app/(auth)/account/setup/page.tsx
@@ -1,12 +1,12 @@
 import { NavigationGuardProvider } from "next-navigation-guard";
 
-import { AccountSetup } from "./_/components/account-setup";
+// import { AccountSetup } from "./_/components/account-setup";
 
 export default function Page() {
   return (
     <NavigationGuardProvider>
       <main className="w-full flex-1 flex flex-col gap-12">
-        <AccountSetup />
+        {/* <AccountSetup /> */}
       </main>
     </NavigationGuardProvider>
   );
